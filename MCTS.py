4f7b050f77e8aadada4a27c19cc3c4bba6b78413
Daniel Hernandez <danielhp95@gmail.com>
2018-02-04 13:27:58 +0000

    final push for master code

--- MCTS.py
+++ MCTS.py
@@ -17,10 +17,9 @@ class GameState:
         Un GameState representa una configuracion valida del 'estado' de un juego.
         Por ejemplo, las posiciones de todas las piezas activas en una partida de ajedrez.
         Las funciones presentadas en esta clase son las minimas necesarias
-        para la implementacion del algoritmo UCT para cualquier juego de 2-jugadores,
-        de informacion completa, deterministico y zero-sum.
-        (mas informacion sobre estos terminos en https://github.com/Danielhp95/mcts-workshop)
+        para la implementacion del algoritmo UCT para cualquier juego de 2-jugadores.
 
+        Esta clase es una interfaz. Su uso es puramente ilustrativo. Trabajad con la clase Connect4State 
     """
 
     def __init__(self):
@@ -35,7 +34,7 @@ class GameState:
 
     def DoMove(self, move):
         """ Transforma el GameState al llevar a cabo el movimiento 'move'.
-            Es importante que se actualize playerJustMoved.
+            Es importante que se actualize playerJustMoved, la variable que indica a quien le toca jugar.
 
             @input move: (int) accion tomada por el agente
         """
@@ -69,16 +68,22 @@ class Connect4State:
 
     def __init__(self, width=7, height=6):
         self.playerJustMoved = 2  # Al empezar el juego se considera que el jugador 2 ha hecho un movimiento. Que es equivalente a decir que el jugador 1 empieza.
-        self.board = []  # 0 = vacio, 1 = jugador 1 (X), 2 = jugador 2 (O)
         self.winner = 0 # 0 = ningun ganador, 1 = jugador 1 ha ganado, 2 = jugador 2 ha ganado.
 
         self.width = width
         self.height = height
+        self.InitializeBoard()
+
+    def InitializeBoard(self):
+        """ Inicializa el tablero de juego, la representacion interna del estado del tablero.
+        """
+        self.board = []  # 0 = vacio, 1 = jugador 1 (X), 2 = jugador 2 (O)
         for y in range(self.width):
             self.board.append([0] * self.height) # Genera una fila de 0s. Generando un tablero vacio
 
     def Clone(self):
-        """ Crea una copia profunda del este GameState
+        """ Crea una copia profunda del este GameState. Usado en las simulaciones 
+            dado que el GameState de la simulacion tiene que ser diferente al GameState de la partida real.
         """
         st = Connect4State(width=self.width, height=self.height)
         st.playerJustMoved = self.playerJustMoved
@@ -91,6 +96,7 @@ class Connect4State:
             Es importante que se actualize playerJustMoved.
 
             @input movecol: (int) columna sobre la que se va a soltar la pieza.
+            @output void
         """
 
         assert movecol >= 0 and movecol <= self.width and self.board[movecol][self.height - 1] == 0
@@ -118,6 +124,8 @@ class Connect4State:
 
             @input x: index de la columna
             @input y: index de la fila
+
+            @output doesMoveWin: (boolean) True si el ultimo movimiento ha ganado la partida
         """
         me = self.board[x][y]
         for (dx, dy) in [(0, +1), (+1, +1), (+1, 0), (+1, -1)]:
@@ -143,8 +151,13 @@ class Connect4State:
         """ Devuelve el ganador de la partida desde el punto de vista de playerJustMoved.
 
             @input playerJustMoved: (int) numero del jugador que acaba de tomar una accion.
+
+            @output: (int) 1 si el ultimo jugador en tomar una accion ha ganado la partida, 0 en cualquier otro caso.
         """
         return playerJustMoved == self.winner
+
+    def IsGameOver(self):
+        return self.GetMoves() == []
 
     def __repr__(self):
         s = ""
@@ -177,6 +190,7 @@ class Node:
         """ Usa la formula de UCB1 para seleccionar uno de los nodos hijo:
                 lambda child: child.wins/child.visits + const * sqrt(2*log(self.visits)/child.visits)
             la constante se usa para escoger entre exploracion o explotacion.
+            En este caso, const = 1
         """
         s = sorted(self.childNodes, key=lambda c: c.wins / c.visits + sqrt(2 * log(self.visits) / c.visits))[-1]
         return s
@@ -187,11 +201,13 @@ class Node:
 
             @input move: (int) accion tomada por el agente
             @input state: (GameState) estado correspondiente al nuevo nodo hijo (childNode)
+
+            @output node
         """
         node = Node(move=move, parent=self, state=state)
         self.untriedMoves.remove(move)
         self.childNodes.append(node)
-        return n
+        return node
 
     def Update(self, result):
         """ Actualiza las estadisticas guardadas en este nodo.
@@ -199,6 +215,7 @@ class Node:
             One additional visit and result additional wins. Result must be from the viewpoint of playerJustmoved.
 
             @input result: (int) 1 denota victoria, 0 para el resto de los casos.
+            @output void
         """
         self.visits += 1
         self.wins += result
@@ -229,11 +246,12 @@ class Node:
 
 
 def UCT(rootstate, itermax, verbose=False):
-    """ Conduct a UCT search for itermax iterations starting from rootstate.
-        Return the best move from the rootstate.
-        Assumes 2 alternating players (player 1 starts), with game results in the range [0.0, 1.0].
+    """ Conduce una busqueda con el algoritmo MCTS-UCT durante itermax iteraciones empezando desde rootstate.
+        Assume 2 jugadores que se alternan, con resultados finales en el rango [0.0, 1.0].
 
         @input itermax: (int) numero de simulaciones (partidas) que se van a ejecutar antes de decidir que accion tomar.
+
+        @output move: (int) accion que llevar a cabo este turno.
     """
 
     rootnode = Node(state=rootstate)
@@ -242,28 +260,39 @@ def UCT(rootstate, itermax, verbose=False):
         node = rootnode
         state = rootstate.Clone()
 
-        # Select
+        '''
+        Part 1.1: Fase Seleccion
+        '''
         while node.untriedMoves == [] and node.childNodes != []:  # Mientras el nodo este completamente expandido y non sea terminal
             node = node.UCTSelectChild()
             state.DoMove(node.move)
 
-        # Expand
+
+        '''
+        part 1.2: fase seleccion
+        '''
         if node.untriedMoves != []:  # if we can expand (i.e. state/node is non-terminal)
             m = random.choice(node.untriedMoves)
             state.DoMove(m)
             node = node.AddChild(m, state)  # add child and descend tree
 
-        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
+        '''
+        part 1.3: fase Simulacion
+        '''
         while state.GetMoves() != []:  # while state is non-terminal
             state.DoMove(random.choice(state.GetMoves()))
 
+        '''
+        part 1.3: fase Retropropagacion
+        '''
         # Backpropagate
         while node is not None:  # backpropagate from the expanded node and work back to the root node
             node.Update(state.GetResult(node.playerJustMoved))  # Update node with result from POV of node.playerJustMoved
             node = node.parentNode
 
-        # Selection # return the move that was most visited
-        # move = sorted(rootnode.childNodes, key=lambda c: c.visits)[-1].move
+        '''
+        part 1.3: fase Seleccion de accion
+        '''
         move = sorted(rootnode.childNodes, key=lambda c: c.wins / c.visits)[-1].move
 
     # Output some information about the tree - can be omitted
@@ -279,22 +308,26 @@ def UCT(rootstate, itermax, verbose=False):
 """
 
 
-def UCTPlayGame():
+def PlayGame(initialState):
     """ Play a sample game between two UCT players where each player gets a different number
         of UCT iterations (= simulations = tree nodes).
     """
-    state = Connect4State(width=7, height=6)
-    while (state.GetMoves() != []):  # while not terminal state
+    state = initialState
+    while not state.IsGameOver():  # while not terminal state
         print(str(state))
-        print(state.playerJustMoved)
         if state.playerJustMoved == 1:
             # JUGADOR 2
-            m = human_input(state)
+            m = HumanInput(state)
         else:
             # JUGADOR 1
             m = UCT(rootstate=state, itermax=10000, verbose=False)  # play with values for itermax and verbose = True
-        print("Best Move: " + str(m) + "\n")
         state.DoMove(m)
+    PrintResults(state)    
+
+def PrintResults(state):
+    """ Imprime los resultados de la partida una vez terminada
+        Esta funcion asume que la partida ya ha terminado
+    """
     if state.GetResult(state.playerJustMoved) == 1.0:
         print(str(state))
         print("Player " + str(state.playerJustMoved) + " wins!")
@@ -305,8 +338,12 @@ def UCTPlayGame():
         print("Nobody wins!")
 
 
-# Toma input para el juego desde standard input. Permite jugar a humanos.
-def human_input(state):
+def HumanInput(state):
+    """  Toma input para el juego desde standard input. Permite jugar a humanos.
+	 Haz nota de como los indices de las columans van de [0,6], pero el input se toma de [1,7]
+	 Esto se hace para que sea mas comodo jugar desde el teclado
+    """
+
     valid_moves = state.GetMoves()
     move = None
     while move not in valid_moves:
@@ -315,8 +352,6 @@ def human_input(state):
         move -= 1
     return move
 
-
 if __name__ == "__main__":
     colorama.init()  # Initiates colorma for color terminal text. This is required for windows machines
-    """ Play a single game to the end using UCT for both players."""
-    UCTPlayGame()
+    PlayGame(Connect4State(width=7, height= 6)) # Comienza el juego!
